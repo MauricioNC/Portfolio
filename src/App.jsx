cddc7f11f023782a77622c78d17f8496ea8811ab
MauricioNC <mauricionc19@gmail.com>
2024-02-27 11:54:33 -0600

    feat: Removed unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-red-500'>Hola mundo desde React</h1>
     </>
   )
 }
