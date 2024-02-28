23d6578d722cc29345eeb75f087dadcb99fd40f3
MauricioNC <mauricionc19@gmail.com>
2024-02-27 21:43:14 -0600

    feat: Layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Experience from './components/Experience'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
+import './index.css'
 
 function App() {
   return (
-    <main>
+    <main className='w-full sm:col-span-12 md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8'>
       <Hero />
       <Experience />
       <Projects />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,16 @@ import profile from '../assets/images/profile.jpg'
 
 function Hero() {
   return (
-    <>
-      <article>
+    <article>
+      <div>
         <h1>Hi! I'm Mauricio</h1>
         <p>
           Back-end developer with{' '}
           <span>Ruby on Rails living in &#x1F1F2;&#x1F1FD;</span>
         </p>
-      </article>
+      </div>
       <img src={profile} alt='User profile image' width={200} />
-    </>
+    </article>
   )
 }
 
