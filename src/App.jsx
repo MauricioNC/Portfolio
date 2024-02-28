1c7c619be28024e6cef420878dbe7d9d0dfa3ebd
MauricioNC <mauricionc19@gmail.com>
2024-02-27 21:46:02 -0600

    fix: Fixed layout style on main tag

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 function App() {
   return (
-    <main className='w-full sm:col-span-12 md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8'>
+    <main className='w-full col-span-12 md:col-start-2 md:col-span-10 lg:col-start-3 lg:col-span-8'>
       <Hero />
       <Experience />
       <Projects />
