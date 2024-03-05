3c45a4e4dbb05399f5c8f02131848d00354df687
MauricioNC <mauricionc19@gmail.com>
2024-03-04 19:02:10 -0600

    feat: Modified Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import Heart from './icons/Heart'
 function Footer() {
   const date = new Date()
   return (
-    <footer className='py-8'>
-      <div className='text-sm text-[#a0a5a7b9] font-medium flex flex-row justify-between items-start flex-wrap'>
+    <footer className='py-8 text-sm text-[#a0a5a7b9] font-medium'>
+      <div className='flex flex-row justify-between items-start flex-wrap'>
         <div className='flex flex-row gap-1 flex-wrap'>
           Made without <Heart /> by Mauricio Navarro
         </div>
@@ -12,6 +12,21 @@ function Footer() {
           <span>&copy; </span>
           {date.getFullYear()}
         </div>
+      </div>
+      <div>
+        <p>
+          This portfolio is based on{' '}
+          <span className='font-bold'>midudev's</span> portfolio and{' '}
+          <span>
+            <a
+              href='https://www.behance.net/gallery/191441155/Personal-Protfolio'
+              target='_blank'
+              className='font-bold'>
+              jonaidjonaid3{' '}
+            </a>
+          </span>
+          design
+        </p>
       </div>
     </footer>
   )
