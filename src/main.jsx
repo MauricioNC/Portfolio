4a4f308dcd0c71a66c90b512e35a1c969da2144f
MauricioNC <mauricionc19@gmail.com>
2024-03-06 13:42:14 -0600

    feat: Added background gradient

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='absolute w-full h-full bg-transparent top-0 left-0 right-0 flex items-start justify-center z-[-1]'>
+      <div className="w-full h-full relative before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[radial-gradient(circle_1200px_at_50%_50%,rgba(220,20,60,0.1),rgba(19,21,22,1))] flex flex-col items-center justify-top z-20"></div>
+    </div>
     <App />
   </React.StrictMode>,
 )
