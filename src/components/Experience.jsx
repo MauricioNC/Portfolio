52a49f8df3ad8590c7fa306e990401fc92924841
nlinedev <newline.dev1@gmail.com>
2024-08-03 22:36:29 -0600

    feat: Rendering job experience from json data.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,5 @@
+import ExperienceData from "../data/experience"
+
 function Experience() {
   return (
     <section
@@ -10,32 +12,31 @@ function Experience() {
         </h3>
         <span className="relative w-[60px] h-[0.18rem] bg-[crimson] flex items-center justify-end before:content-[''] before:absolute before:w-[10px] before:h-[10px] before:bg-[crimson] before:rounded-full"></span>
       </div>
-      <div className="experience_item self-center relative flex flex-col md:flex-row justify-items-stretch gap-8 md:justify-start before:content-[''] before:w-[0.15rem] before:h-full before:bg-[#4e4e4e9f] before:absolute before:left-0 pl-10 pb-14">
-        <div className='grow relative'>
-          <h3 className='text-balance text-[crimson]'>Engineer designer</h3>
-          <h4 className='text-balance'>Tata Consultancy Services</h4>
-          <span>Jan 2021 - Oct 2023</span>
+      <div className="experience_item self-center relative before:content-[''] before:w-[0.15rem] before:h-full before:bg-[#4e4e4e9f] before:absolute before:left-0 pl-10">
+        <div className="flex flex-col gap-14 w-full">
+          {
+            ExperienceData.map((job) => {
+              return (
+                <div className="flex flex-row justify-items-stretch" key={(job.jobTitle+job.jobCompany)}>
+                  <span className='absolute w-3 h-3 bg-[crimson] -left-[0.30rem] rounded-full'></span>
+                  <div className='grow relative'>
+                    <h3 className='text-balance text-[crimson]'>{job.jobTitle}</h3>
+                    <h4 className='text-balance'>{job.jobCompany}</h4>
+                    <span>{job.date}</span>
+                  </div>
+                  <div className='self-start md:self-center flex flex-col items-start md:basis-[500px]'>
+                    <p className='line-clamp-4 text-left'>
+                      {job.jobDescription}
+                    </p>
+                    <button className='my-3 p-2 bg-[crimson] rounded-md hover:contrast-[1.1]'>
+                      Read more
+                    </button>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
-        <div className='self-start md:self-center flex flex-col items-start md:basis-[500px]'>
-          <p className='line-clamp-4 text-left'>
-            I was member of the J&J team, primarily focused on assisting the
-            development team in configuring information in the Camstar platform.
-            My role involved leveraging Excel-based information on medical
-            devices to tailor the system to the production team's needs.
-            Utilizing Scrum methodology, our tasks and activities successfully
-            completed, followed by rigorous testing by the QA team. In case of
-            any issue identified during this process, my team and I went back to
-            work into the task to get the issue solved. Also, sometimes I helped
-            to my lead to test some configuration in the platform using basic
-            SQL queries that let us identify for wrong outputs or if the
-            production team's needed a new configuration, we had to create a new
-            query to tailor the system to the production team's needs.
-          </p>
-          <button className='my-3 p-2 bg-[crimson] rounded-md hover:contrast-[1.1]'>
-            Read more
-          </button>
-        </div>
-        <span className='absolute w-3 h-3 bg-[crimson] -left-[0.30rem] top-6 rounded-full'></span>
       </div>
     </section>
   )
